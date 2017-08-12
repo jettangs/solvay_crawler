f9a717cd9497b2135b090c4378977cb383cf2c98
jettangs <jettangs@gmail.com>
2017-08-12 19:59:14 +0800

    initial

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ const News = sequelize.define('news', {
     news['author'] = 'solvay'
     console.log(news.author)
 
-    news['cover'] = he.decode(article.eq(i).find('.main-picture').find('img').attr('src').substr(1))
+    news['cover'] = he.decode(article.eq(i).find('.main-picture').find('img').attr('src'))
     console.log(news.cover)
 
     news['host'] = 'www.solvay.com'
